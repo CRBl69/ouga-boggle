d789c3fac2e24b04041ae350a6c0f0eb563f5652
Bogdan Crisan <crbl@cock.li>
2022-03-10 12:37:55 +0100

    Ajout de tests.

--- src/test/java/com/boggle/serveur/plateau/MotTest.java
+++ src/test/java/com/boggle/serveur/plateau/MotTest.java
@@ -1,5 +1,7 @@
 package com.boggle.serveur.plateau;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import com.boggle.serveur.dictionnaire.Dictionnaire;
@@ -16,12 +18,14 @@ public class MotTest {
         lettres.add(new Lettre(new Coordonnee(0, 1), "e"));
         lettres.add(new Lettre(new Coordonnee(0, 2), "s"));
         lettres.add(new Lettre(new Coordonnee(0, 3), "t"));
+
+        var erreur = false;
         try {
             new Mot(lettres);
-            assertTrue(true);
         } catch (Exception e) {
-            assertTrue(false);
+            erreur = true;
         }
+        assertFalse(erreur);
     }
 
     @Test
@@ -33,12 +37,14 @@ public class MotTest {
         lettres.add(new Lettre(new Coordonnee(0, 2), "s"));
         lettres.add(new Lettre(new Coordonnee(0, 3), "t"));
         lettres.add(new Lettre(new Coordonnee(0, 4), "t"));
+
+        var erreur = false;
         try {
             new Mot(lettres);
-            assertTrue(false);
         } catch (Exception e) {
-            assertTrue(true);
+            erreur = true;
         }
+        assertTrue(erreur);
     }
 
     @Test
@@ -49,11 +55,51 @@ public class MotTest {
         lettres.add(new Lettre(new Coordonnee(0, 1), "e"));
         lettres.add(new Lettre(new Coordonnee(0, 2), "s"));
         lettres.add(new Lettre(new Coordonnee(0, 3), "t"));
-        try {
-            new Mot(lettres).toString().equals("test");
-            assertTrue(true);
-        } catch (Exception e) {
-            assertTrue(false);
-        }
+
+        Mot mot = new Mot(lettres);
+
+        assertEquals("test", mot.toString());
+    }
+
+    @Test
+    public void fonctionGetLettres() {
+        Dictionnaire.genererArbre(Langue.FR);
+        LinkedList<Lettre> lettres = new LinkedList<>();
+        lettres.add(new Lettre(new Coordonnee(0, 0), "t"));
+        lettres.add(new Lettre(new Coordonnee(0, 1), "e"));
+        lettres.add(new Lettre(new Coordonnee(0, 2), "s"));
+        lettres.add(new Lettre(new Coordonnee(0, 3), "t"));
+
+        Mot mot = new Mot(lettres);
+
+        assertEquals("t", mot.getLettres().get(0).lettre);
+        assertEquals("e", mot.getLettres().get(1).lettre);
+        assertEquals("s", mot.getLettres().get(2).lettre);
+        assertEquals("t", mot.getLettres().get(3).lettre);
+    }
+
+    @Test
+    public void fonctionGetPoints() {
+        Dictionnaire.genererArbre(Langue.FR);
+        LinkedList<Lettre> lettres = new LinkedList<>();
+        lettres.add(new Lettre(new Coordonnee(0, 0), "t"));
+        lettres.add(new Lettre(new Coordonnee(0, 1), "e"));
+        lettres.add(new Lettre(new Coordonnee(0, 2), "s"));
+        lettres.add(new Lettre(new Coordonnee(0, 3), "t"));
+
+        Mot mot = new Mot(lettres);
+
+        LinkedList<Lettre> lettres2 = new LinkedList<>();
+        lettres2.add(new Lettre(new Coordonnee(0, 0), "m"));
+        lettres2.add(new Lettre(new Coordonnee(0, 1), "a"));
+        lettres2.add(new Lettre(new Coordonnee(0, 2), "i"));
+        lettres2.add(new Lettre(new Coordonnee(0, 3), "s"));
+        lettres2.add(new Lettre(new Coordonnee(0, 4), "o"));
+        lettres2.add(new Lettre(new Coordonnee(0, 5), "n"));
+
+        Mot mot2 = new Mot(lettres2);
+
+        assertEquals(1, mot.getPoints());
+        assertEquals(3, mot2.getPoints());
     }
 }
